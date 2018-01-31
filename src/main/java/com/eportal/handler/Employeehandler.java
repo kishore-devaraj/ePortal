4be6e9c9b3756f0a85e0f3b4cf0b78cfa2e43089
kishore-devaraj <kishoregrylls@gmail.com>
2018-01-31 12:19:45 +0530

    Deleted unused files

--- src/main/java/com/eportal/handler/Employeehandler.java
+++ src/main/java/com/eportal/handler/Employeehandler.java
@@ -1,17 +1,22 @@
 package com.eportal.handler;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.eportal.models.Employee;
 import com.eportal.service.CreateResource;
+import com.eportal.service.DeleteResource;
+import com.eportal.service.UpdateResource;
 import com.eportal.utils.GenericResponse;
 
-@Path("/create")
-public class RegisterEmployee {
+@Path("/")
+public class Employeehandler {
 	
 	@POST
 	@Path("/employee")
@@ -39,6 +44,47 @@ public class RegisterEmployee {
 			response.setData("error", "Error happened in processing the data");
 		}
 		
+		return response;
+	}
+	
+	
+	
+	@DELETE
+	@Path("/{org}/employee/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public GenericResponse deleteEmployee(
+			@PathParam("id") String id,
+			@PathParam("org") String org
+			){
+		GenericResponse response = new GenericResponse();
+		if (id != null){
+			DeleteResource delete = new DeleteResource();
+			response = delete.Employee(org,id,response);
+		}else{
+			response.setCode(400);
+			response.setData("error", "Specify in the Employee Id in the path param");
+		}
+		return response;	
+	}
+	
+	
+	
+	@PUT
+	@Path("/employee")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public GenericResponse updateEmployee(Employee employee){
+		GenericResponse response = new GenericResponse();
+		UpdateResource update = new UpdateResource();
+		
+		if ((employee.getOrganisation() !=  null) && 
+			(employee.getEmployeeId() != null)){
+			response = update.Employee(employee, response);
+	
+		}else{
+			response.setCode(400);
+			response.setData("error","EmployeeId and Organisation is must");
+		}
 		return response;
 	}
 }
